3a1d2b581ae970e3d78b5a83e05340715ce07e5f
David Francis <davidjayfrancis@gmail.com>
2019-09-14 10:32:52 -0700

    MVP

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,49 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+/*
+authorName: "FIDO WALKSALOT"
+authorPhoto: "./assets/fido.jpg
+headline: "Bootstrap 5: Get a Sneak Peak at all the New Features"
+*/
+cardsContainer = document.querySelector(".cards-container");
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    // console.log(response.data.articles.bootstrap) == array of article objects
+    response.data.articles.bootstrap.forEach(item => {
+      newCard = Card(item);
+      cardsContainer.appendChild(newCard);
+    });
+  });
+
+function Card(obj) {
+  // create elements
+  card = document.createElement("div");
+  headline = document.createElement("div");
+  author = document.createElement("div");
+  imgContainer = document.createElement("div");
+  img = document.createElement("img");
+  span = document.createElement("span");
+
+  //add classes
+  card.classList.add("card");
+  headline.classList.add("headline");
+  author.classList.add("author");
+  imgContainer.classList.add("img-container");
+  imgContainer.appendChild(img);
+
+  //build structure
+  card.appendChild(headline);
+  card.appendChild(author);
+  author.appendChild(imgContainer);
+  author.appendChild(span);
+
+  // build content from object
+  headline.textContent = obj.headline;
+
+  span.textContent = `By ${obj.authorName}`;
+  img.src = obj.authorPhoto;
+  return card;
+}
